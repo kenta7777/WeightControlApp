a2ef122c9e0ed4febc3ac79dc3a0eba79d4faa9a
kenta7777 <k.hasegw7@gmail.com>
2020-09-09 09:55:01 +0900

    [weight-record-ui] add edittext for inputting user's weight(error handling is not implemented)

--- app/src/main/java/com/example/weightcontrolapp/MainActivity.kt
+++ app/src/main/java/com/example/weightcontrolapp/MainActivity.kt
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.weightcontrolapp.ui.weightrecord.WeightRecordFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.WeightRecordFragment, WeightRecordFragment())
+//            .commit()
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
--- app/src/main/java/com/example/weightcontrolapp/ui/weightrecord/WeightRecordFragment.kt
+++ app/src/main/java/com/example/weightcontrolapp/ui/weightrecord/WeightRecordFragment.kt
@@ -1,25 +1,68 @@
 package com.example.weightcontrolapp.ui.weightrecord
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.weightcontrolapp.R
+import com.example.weightcontrolapp.R.layout.weight_record_fragment
+import kotlinx.android.synthetic.main.weight_record_fragment.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class WeightRecordFragment : Fragment() {
 
+    private val viewModel: WeightRecordViewModel by activityViewModels()
+
+    private lateinit var weightRecordInput: EditText
+
+    private val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            val weightRecordString = s.toString()
+
+            if (weightRecordString.isEmpty()) {
+                //TODO Error Handling
+            }
+        }
+    }
+
+    private var root: View? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //viewModel.userWeightText.value = weight_text_input.text.toString()
+//        val binding = DataBindingUtil.setContentView(this, R.layout.weight_record_fragment)
+
+    }
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.weight_record_fragment, container, false)
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        val editText = EditText(requireContext())
+
+        return inflater.inflate(weight_record_fragment, container, false)
+
+
+        //val weightEditText = v?.findViewById<EditText>(R.id.weight_text_input)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -30,7 +73,6 @@ class WeightRecordFragment : Fragment() {
         }
     }
 
-    //TODO: Get weight record from EditText
     //TODO: Show the value in CheckWeightRecordFragment
     //TODO: Deal with abnormal value(e.g blank space) in weight record EditText
 }
