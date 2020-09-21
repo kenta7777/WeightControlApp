4afc9a79db49e61d18dd3ba727783b9fd612c939
kenta7777 <k.hasegw7@gmail.com>
2020-09-22 02:01:34 +0900

    [add-local-db] Add functions which calles function defined in WeightDataDao

--- app/src/main/java/com/example/weightcontrolapp/ui/weightrecord/WeightRecordViewModel.kt
+++ app/src/main/java/com/example/weightcontrolapp/ui/weightrecord/WeightRecordViewModel.kt
@@ -2,7 +2,22 @@ package com.example.weightcontrolapp.ui.weightrecord
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.weightcontrolapp.MainActivity
+import com.example.weightcontrolapp.data.model.db.WeightData
 
 class WeightRecordViewModel : ViewModel() {
     var userWeightText = MutableLiveData<String>()
+    // Instantiate WeightDataDao
+    private val dao = MainActivity.db.weightDataDao()
+
+    // This method is called in WeightRecordFragment
+    fun loadAllWeightDataList() {
+        dao.loadAllWeightData()
+    }
+
+    // This method is called in WeightRecordFragment
+    fun loadWeightData(weightData: WeightData) {
+        dao.insertWeightData(weightData)
+    }
+
 }
