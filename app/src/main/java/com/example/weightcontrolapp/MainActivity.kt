bb0bcff9881932d4a1fccfe8fcd8f1b49ff7c9c6
kenta7777 <k.hasegw7@gmail.com>
2020-09-27 17:39:06 +0900

    [add-local-db] Allow for destructive migration of DB

--- app/src/main/java/com/example/weightcontrolapp/MainActivity.kt
+++ app/src/main/java/com/example/weightcontrolapp/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
             applicationContext,
             AppDatabase::class.java,
             "app_database"
-        ).build()
+        ).fallbackToDestructiveMigration().build()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/example/weightcontrolapp/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/example/weightcontrolapp/data/local/db/AppDatabase.kt
@@ -7,7 +7,7 @@ import com.example.weightcontrolapp.data.DateConverters
 import com.example.weightcontrolapp.data.local.db.dao.WeightDataDao
 import com.example.weightcontrolapp.data.model.db.WeightData
 
-@Database(entities = [WeightData::class], version = 1)
+@Database(entities = [WeightData::class], version = 2)
 @TypeConverters(DateConverters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun weightDataDao(): WeightDataDao
