55bc1c114cee0128f4e6b22cbe15a7167fc8623c
kenta7777 <k.hasegw7@gmail.com>
2020-10-01 01:20:04 +0900

    [history-display-ui] Enable to show history_fragment layout in HistoryFragment

--- app/src/main/java/com/example/weightcontrolapp/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/example/weightcontrolapp/ui/history/HistoryFragment.kt
@@ -1,12 +1,26 @@
 package com.example.weightcontrolapp.ui.history
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.weightcontrolapp.R
 
 class HistoryFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        return inflater.inflate(R.layout.history_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
