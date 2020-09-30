3d1277bd413ab587237e954bc822d3d9440d6231
kenta7777 <k.hasegw7@gmail.com>
2020-09-30 09:42:14 +0900

    [history-display-ui] Create HistoryFragment UI

---
+++ app/src/main/java/com/example/weightcontrolapp/ui/history/HistoryFragment.kt
@@ -0,0 +1,19 @@
+package com.example.weightcontrolapp.ui.history
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class HistoryFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+}
