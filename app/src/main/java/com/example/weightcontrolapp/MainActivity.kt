9734fb04cd5e23cf782bebe807352235acbd6f65
kenta7777 <k.hasegw7@gmail.com>
2020-10-05 23:47:56 +0900

    [history-display-ui] Add DrawerLayout for navigating each fragment

--- app/src/main/java/com/example/weightcontrolapp/MainActivity.kt
+++ app/src/main/java/com/example/weightcontrolapp/MainActivity.kt
@@ -1,28 +1,63 @@
 package com.example.weightcontrolapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.room.Room
 import com.example.weightcontrolapp.data.local.db.AppDatabase
+import com.example.weightcontrolapp.ui.history.HistoryFragment
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
     companion object {
         lateinit var db: AppDatabase
     }
 
+    private lateinit var navController : NavController
+    private lateinit var appBarConfiguration : AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        navController = findNavController(R.id.nav_host_fragment)
+        findViewById<NavigationView>(R.id.navigation_view).setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawer_layout)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
         db = Room.databaseBuilder(
             applicationContext,
             AppDatabase::class.java,
             "app_database"
         ).fallbackToDestructiveMigration().build()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
